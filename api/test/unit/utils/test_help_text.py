rocktec57/wp_openverse

commit 044f2984ce5c21a196bc3b529d7ff82ac8403c61

Replace plural `categories` as field name with singular `category` (#583)
* Replace plural `categories` as field name with singular `category`

Signed-off-by: Olga Bulat <obulat@gmail.com>

* Replace `categories` in the search controller

Signed-off-by: Olga Bulat <obulat@gmail.com>

* Make `categories` work, too

Signed-off-by: Olga Bulat <obulat@gmail.com>

* Add test for category filtering (#584)

* Update api/test/audio_integration_test.py

Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

Co-authored-by: Krystle Salazar <krystle.salazar@ciens.ucv.ve>
Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

api/catalog/api/serializers/audio_serializers.py
@@ -52,7 +52,7 @@ class AudioSearchRequestSerializer(
     fields_names = [
         *MediaSearchRequestSerializer.fields_names,
         *AudioSearchRequestSourceSerializer.field_names,
-        "categories",
+        "category",
         "duration",
     ]
     """
@@ -60,8 +60,8 @@ class AudioSearchRequestSerializer(
     used to generate Swagger documentation.
     """
 
-    categories = serializers.CharField(
-        label="categories",
+    category = serializers.CharField(
+        label="category",
         help_text="A comma separated list of categories; available categories "
         "include `music`, `sound_effect`, `podcast`, `audiobook`, "
         "and `news`.",

api/catalog/api/serializers/image_serializers.py
@@ -26,7 +26,7 @@ class ImageSearchRequestSerializer(
     fields_names = [
         *MediaSearchRequestSerializer.fields_names,
         *ImageSearchRequestSourceSerializer.field_names,
-        "categories",
+        "category",
         "aspect_ratio",
         "size",
     ]
@@ -36,8 +36,8 @@ class ImageSearchRequestSerializer(
     """
 
     # Ref: ingestion_server/ingestion_server/categorize.py#Category
-    categories = serializers.CharField(
-        label="categories",
+    category = serializers.CharField(
+        label="category",
         help_text="A comma separated list of categories; available categories "
         "include `illustration`, `photograph`, and "
         "`digitized_artwork`.",

api/test/audio_integration_test.py
@@ -10,6 +10,7 @@ from test.media_integration import (
     report,
     search,
     search_all_excluded,
+    search_by_category,
     search_consistency,
     search_quotes,
     search_source_and_excluded,
@@ -32,6 +33,11 @@ def audio_fixture():
 
 def test_search(audio_fixture):
     search(audio_fixture)
+
+
+def test_search_category_filtering(audio_fixture):
+    search_by_category("audio", "music", audio_fixture)
+    search_by_category("audio", "pronunciation", audio_fixture)
 
 
 def test_search_all_excluded():

api/catalog/api/controllers/search_controller.py
@@ -239,7 +239,8 @@ def search(
     # names are identical.
     filters = [
         ("extension", None),
-        ("categories", None),
+        ("category", None),
+        ("categories", "category"),
         ("aspect_ratio", None),
         ("size", None),
         ("source", None),

api/test/media_integration.py
@@ -14,6 +14,16 @@ def search(fixture):
     assert fixture["result_count"] > 0
 
 
+def search_by_category(media_path, category, fixture):
+    response = requests.get(f"{API_URL}/v1/{media_path}?category={category}")
+    assert response.status_code == 200
+    data = json.loads(response.text)
+    assert data["result_count"] < fixture["result_count"]
+    results = data["results"]
+    # Make sure each result is from the specified category
+    assert all(audio_item["category"] == category for audio_item in results)
+
+
 def search_all_excluded(media_path, excluded_source):
     response = requests.get(
         f"{API_URL}/v1/{media_path}?q=test&excluded_source={','.join(excluded_source)}"

commit 5e360b732cc6d6e641ccf4e7d2cf8e7fdc633edb

Move media type categories to constants module (#589)
* Use a TextChoices enum for media type categories

* Remove explicit help text, defer to DRF's automatically generated one

* Simplify categories

* Make categories a fancy set-like object that generates its own help

* Change validator name to reference correct field name

Fixes an issue introduced in #583

* Add tests for make_help_text

* Force sorted order when generating help text

* Generalize help text, attempt 1

* Generalize help text, functional approach

* Apply comma separated list help text to all fields

This also adds validation for image sizes

* Get sources again in function

* Fix validation text

* Update documentation

api/catalog/api/constants/field_values.py
@@ -1,0 +1,31 @@
+AUDIO_CATEGORIES = {
+    "audiobook",
+    "music",
+    "news",
+    "podcast",
+    "pronunciation",
+    "sound_effect",
+}
+
+IMAGE_CATEGORIES = {
+    "digitized_artwork",
+    "illustration",
+    "photograph",
+}
+
+ASPECT_RATIOS = {
+    "tall",
+    "wide",
+    "square",
+}
+
+IMAGE_SIZES = {
+    "small",
+    "medium",
+    "large",
+}
+
+DURATION = {
+    "short",
+    "long",
+}

api/catalog/api/serializers/audio_serializers.py
@@ -1,3 +1,4 @@
+from catalog.api.constants.field_values import AUDIO_CATEGORIES, DURATION
 from catalog.api.docs.media_docs import fields_to_md
 from catalog.api.models import AudioReport
 from catalog.api.models.audio import Audio
@@ -9,6 +10,7 @@ from catalog.api.serializers.media_serializers import (
     _validate_enum,
     get_search_request_source_serializer,
 )
+from catalog.api.utils.help_text import make_comma_separated_help_text
 from elasticsearch_dsl.response import Hit
 from rest_framework import serializers
 
@@ -62,35 +64,23 @@ class AudioSearchRequestSerializer(
 
     category = serializers.CharField(
         label="category",
-        help_text="A comma separated list of categories; available categories "
-        "include `music`, `sound_effect`, `podcast`, `audiobook`, "
-        "and `news`.",
+        help_text=make_comma_separated_help_text(AUDIO_CATEGORIES, "categories"),
         required=False,
     )
     duration = serializers.CharField(
         label="duration",
-        help_text="A comma separated list of audio lengths; available lengths "
-        "include `short`, and `long`.",
+        help_text=make_comma_separated_help_text(DURATION, "audio lengths"),
         required=False,
     )
 
     @staticmethod
-    def validate_categories(value):
-        valid_categories = {
-            "music",
-            "sound_effect",
-            "podcast",
-            "news",
-            "audiobook",
-            "pronunciation",
-        }
-        _validate_enum("category", valid_categories, value)
+    def validate_category(value):
+        _validate_enum("category", AUDIO_CATEGORIES, value)
         return value.lower()
 
     @staticmethod
     def validate_duration(value):
-        valid_durations = {"short", "long"}  # TODO: Finalise duration filters
-        _validate_enum("duration", valid_durations, value)
+        _validate_enum("duration", DURATION, value)
         return value.lower()
 
 

api/catalog/api/serializers/image_serializers.py
@@ -1,3 +1,8 @@
+from catalog.api.constants.field_values import (
+    ASPECT_RATIOS,
+    IMAGE_CATEGORIES,
+    IMAGE_SIZES,
+)
 from catalog.api.docs.media_docs import fields_to_md
 from catalog.api.models import Image, ImageReport
 from catalog.api.serializers.base import SchemableHyperlinkedIdentityField
@@ -9,6 +14,7 @@ from catalog.api.serializers.media_serializers import (
     _validate_enum,
     get_search_request_source_serializer,
 )
+from catalog.api.utils.help_text import make_comma_separated_help_text
 from rest_framework import serializers
 
 
@@ -38,34 +44,33 @@ class ImageSearchRequestSerializer(
     # Ref: ingestion_server/ingestion_server/categorize.py#Category
     category = serializers.CharField(
         label="category",
-        help_text="A comma separated list of categories; available categories "
-        "include `illustration`, `photograph`, and "
-        "`digitized_artwork`.",
+        help_text=make_comma_separated_help_text(IMAGE_CATEGORIES, "categories"),
         required=False,
     )
     aspect_ratio = serializers.CharField(
         label="aspect_ratio",
-        help_text="A comma separated list of aspect ratios; available aspect "
-        "ratios include `tall`, `wide`, and `square`.",
+        help_text=make_comma_separated_help_text(ASPECT_RATIOS, "aspect ratios"),
         required=False,
     )
     size = serializers.CharField(
         label="size",
-        help_text="A comma separated list of image sizes; available sizes"
-        " include `small`, `medium`, or `large`.",
+        help_text=make_comma_separated_help_text(IMAGE_SIZES, "image sizes"),
         required=False,
     )
 
     @staticmethod
-    def validate_categories(value):
-        valid_categories = {"illustration", "digitized_artwork", "photograph"}
-        _validate_enum("category", valid_categories, value)
+    def validate_category(value):
+        _validate_enum("category", IMAGE_CATEGORIES, value)
         return value.lower()
 
     @staticmethod
     def validate_aspect_ratio(value):
-        valid_ratios = {"tall", "wide", "square"}
-        _validate_enum("aspect ratio", valid_ratios, value)
+        _validate_enum("aspect ratio", ASPECT_RATIOS, value)
+        return value.lower()
+
+    @staticmethod
+    def validate_size(value):
+        _validate_enum("size", IMAGE_SIZES, value)
         return value.lower()
 
 

api/catalog/api/serializers/media_serializers.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 
 import catalog.api.licenses as license_helpers
 from catalog.api.controllers.search_controller import get_sources
+from catalog.api.utils.help_text import make_comma_separated_help_text
 from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
@@ -103,9 +104,8 @@ def get_search_request_source_serializer(media_type):
         """
 
         _field_attrs = {
-            "help_text": (
-                "A comma separated list of data sources to search. "
-                f"Valid inputs: {format_enums(get_sources(media_type).keys())}"
+            "help_text": make_comma_separated_help_text(
+                get_sources(media_type).keys(), "data sources"
             ),
             "required": False,
         }
@@ -172,21 +172,21 @@ class MediaSearchRequestSerializer(serializers.Serializer):
 
     q = serializers.CharField(
         label="query",
-        help_text="A query string that should not exceed 200 characters in " "length",
+        help_text="A query string that should not exceed 200 characters in length",
         required=False,
     )
     license = serializers.CharField(
         label="licenses",
-        help_text="A comma-separated list of licenses. Example: `by,cc0`. "
-        "Valid inputs: "
-        f"`{list(license_helpers.LICENSE_GROUPS['all'])}`",
+        help_text=make_comma_separated_help_text(
+            license_helpers.LICENSE_GROUPS["all"], "licenses"
+        ),
         required=False,
     )
     license_type = serializers.CharField(
         label="license type",
-        help_text="A list of license types. "
-        "Valid inputs: "
-        f"`{list(license_helpers.LICENSE_GROUPS.keys())}`",
+        help_text=make_comma_separated_help_text(
+            license_helpers.LICENSE_GROUPS.keys(), "license types"
+        ),
         required=False,
     )
     creator = serializers.CharField(

api/catalog/api/utils/help_text.py
@@ -1,0 +1,22 @@
+from typing import Iterable
+
+
+def make_comma_separated_help_text(items: Iterable[str], name: str) -> str:
+    """
+    Generate help text that describes a comma separated list of items with the name
+    provided. Items are wrapped in backticks, and lists with more than one item will
+    have an "and" added before the final item.
+
+    :param items: iterable of available options for this field
+    :param name: plural name of the list of items (e.g. "categories", "aspect ratios")
+    :return: generated help text
+    """
+    formatted = [f"`{item}`" for item in sorted(items)]
+    # Add an "and" at the end of the list
+    if len(formatted) > 1:
+        formatted[-1] = f"and {formatted[-1]}"
+    help_text = (
+        f"A comma separated list of {name}; available {name} include: "
+        f"{', '.join(formatted)}."
+    )
+    return help_text

api/test/audio_integration_test.py
@@ -43,6 +43,11 @@ def test_search_category_filtering(audio_fixture):
     search_by_category("audio", "pronunciation", audio_fixture)
 
 
+def test_search_category_filtering_fails(audio_fixture):
+    with pytest.raises(AssertionError):
+        search_by_category("audio", "not_valid", audio_fixture)
+
+
 def test_search_all_excluded():
     search_all_excluded("audio", ["jamendo", "wikimedia_audio"])
 

api/test/unit/utils/test_help_text.py
@@ -1,0 +1,27 @@
+import pytest
+from catalog.api.utils.help_text import make_comma_separated_help_text
+
+
+@pytest.mark.parametrize(
+    "items, expected",
+    [
+        ([], "A comma separated list of items; available items include: ."),
+        (
+            ["a"],
+            "A comma separated list of items; available items include: `a`.",
+        ),
+        (
+            ["a", "b"],
+            "A comma separated list of items; "
+            "available items include: `a`, and `b`.",
+        ),
+        (
+            ["a", "b", "c"],
+            "A comma separated list of items; "
+            "available items include: `a`, `b`, and `c`.",
+        ),
+    ],
+)
+def test_make_help_text(items, expected):
+    actual = make_comma_separated_help_text(items, "items")
+    assert actual == expected